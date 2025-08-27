95119ab65edc3e920fc7dcb22338c307ad13c1cc
Mohamed Ismael Abdelhady <mohabdelhady.c@stc.com.sa>
2025-08-27 10:12:35 +0300

    localization

--- apps/shell/src/app/app.config.ts
+++ apps/shell/src/app/app.config.ts
@@ -1,34 +1,55 @@
-import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { CORE_CONFIG, ErrorHandlerService, GlobalHttpErrorInterceptor, HttpBaseInterceptor, TOASTER_SERVICE } from '@corp-products/app-core';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpBackend, provideHttpClient } from '@angular/common/http';
+import { MissingTranslationHandler, provideTranslateService, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { provideToastr } from 'ngx-toastr';
+import { ToasterService } from '@new-rasel/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
 
+    provideHttpClient(),
 
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'en',
+      lang: 'en'
+    }),
+
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
 
     //start core liberary providers
     {
       provide: CORE_CONFIG,
       useValue: {
-        apiPrefix : '',
-        gatewayUrl: '' ,
+        apiPrefix: '',
+        gatewayUrl: '',
       },
     },
-    // {
-    //   provide: TOASTER_SERVICE,
-    //   useClass: ToasterService,
-    // },
+    {
+      provide: TOASTER_SERVICE,
+      useClass: ToasterService,
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpBaseInterceptor,
       multi: true,
     },
-     {
+    {
       provide: ErrorHandler,
       useClass: ErrorHandlerService
     },
@@ -37,6 +58,6 @@ export const appConfig: ApplicationConfig = {
       useClass: GlobalHttpErrorInterceptor,
       multi: true
     },
-     //end core liberary providers
+    //end core liberary providers
   ],
 };
---
+++ libs/core/src/lib/services/toaster.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ToasterInterface, ToasterOptions } from '@corp-products/app-core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ToasterService implements ToasterInterface {
+  constructor(private toastr: ToastrService) {}
+
+  success(options : ToasterOptions): void {
+    this.toastr.success(options.message, options.title);
+  }
+
+  error(options : ToasterOptions): void {
+    this.toastr.error(options.message, options.title);
+  }
+
+  warning(options : ToasterOptions): void {
+    this.toastr.warning(options.message, options.title);
+  }
+
+  info(options : ToasterOptions): void {
+    this.toastr.info(options.message, options.title);
+  }
+}
